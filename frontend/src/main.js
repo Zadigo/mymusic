ae04df36ca8ab07db9e5ff1c73853028403a0a39
Zadigo <pendenquejohn@gmail.com>
2022-06-15 16:13:42 +0200

    Vue i18n

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,10 +7,12 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'mdb-ui-kit/css/mdb.min.css'
 import './plugins/webfontloader'
+import i18n from './i18n'
 
 const pinia = createPinia()
 
 const app = createApp(App)
 app.use(router)
+app.use(i18n)
 app.use(pinia)
 app.mount('#app')
